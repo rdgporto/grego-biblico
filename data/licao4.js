9240d2ccf7e2a7ccbd1030f3544e0b33b5e5e5bf
Rodrigo Costa <rdgporto@gmail.com>
2025-09-23 17:35:49 -0300

    Atualização do vocabulário das lições 4 e 5: adição de novas entradas e significados

--- data/licao4.js
+++ data/licao4.js
@@ -1,15 +1,135 @@
 const vocabularioLicao4 = [
     {
-        grego: "ἄνθρωπος",
-        transliteracao: "anthropos",
-        significado: "homem, ser humano",
-        ocorrencias: 550
+        grego: "ἀγαπάω",
+        transliteracao: "a-ga-PÁ-o",
+        significado: "amar, com consideração, afeição e benevolência",
+        ocorrencias: 143
     },
     {
-        grego: "οἶκος",
-        transliteracao: "oikos",
-        significado: "casa, família",
-        ocorrencias: 105
+        grego: "λαλέω",
+        transliteracao: "la-LÉ-o",
+        significado: "falar, dizer",
+        ocorrencias: 296
+    },
+    {
+        grego: "μέλλω (+ inf.)",
+        transliteracao: "MÉL-lo",
+        significado: "estar prestes a (+inf.), estar a ponto de (+inf.); dever (+inf.)",
+        ocorrencias: 109
+    },
+    {
+        grego: "πληρόω",
+        transliteracao: "ple-RÓ-o",
+        significado: "completar, encher; cumprir",
+        ocorrencias: 87
+    },
+    {
+        grego: "ποιέω",
+        transliteracao: "poi-É-o",
+        significado: "fazer, executar, realizar, trabalhar, produzir",
+        ocorrencias: 568
+    },
+    {
+        grego: "τηρέω",
+        transliteracao: "te-RÉ-o",
+        significado: "guardar, vigiar; observar, obedecer; conservar",
+        ocorrencias: 70
+    },
+    {
+        grego: "ἀλήθεια, -ας, ἡ",
+        transliteracao: "a-LÉ-thei-a, -as, re",
+        significado: "verdade",
+        ocorrencias: 109
+    },
+    {
+        grego: "κύριος, -ου, ὁ",
+        transliteracao: "KÍ-ri-os, -u, rró",
+        significado: "amo, senhor, dono, patrão; Senhor",
+        ocorrencias: 719
+    },
+    {
+        grego: "λόγος, -ου, ὁ",
+        transliteracao: "LÓ-gos, -u, rró",
+        significado: "palavra; discurso, declaração, proclamação; ensino, instrução; mensagem, revelação; assunto, razão",
+        ocorrencias: 330
+    },
+    {
+        grego: "οἶκος, -ου, ὁ",
+        transliteracao: "ÓI-kos, -u, rró",
+        significado: "casa; lar, família; moradia; comunidade (cristã)",
+        ocorrencias: 114
+    },
+    {
+        grego: "πατήρ, πατρός, ὁ",
+        transliteracao: "pa-TÉR, pa-trós, rró",
+        significado: "pai",
+        ocorrencias: 414
+    },
+    {
+        grego: "πνεῦμα, -ατος, τό",
+        transliteracao: "PNÊU-ma, -a-tos, tó",
+        significado: "vento; sopro; espírito; Espírito",
+        ocorrencias: 379
+    },
+    {
+        grego: "σωτήρ, -ῆρος, ὁ",
+        transliteracao: "so-TÉR, -ê-ros, rró",
+        significado: "(< σῴζω) salvador, libertador",
+        ocorrencias: 24
+    },
+    {
+        grego: "δέ (δ᾽)",
+        transliteracao: "DÉ",
+        significado: "partíc. copulativa pospos., advers.: mas, porém, no entanto; ora, então; e, também, além disso; ou seja",
+        ocorrencias: 2801
+    },
+    {
+        grego: "ἐκεῖνος, -η, -ο",
+        transliteracao: "e-KÊI-nos, -e, -o",
+        significado: "adj. e pron. dem.: aquele / aquela / aquilo",
+        ocorrencias: 265
+    },
+    {
+        grego: "ἀνά",
+        transliteracao: "a-NÁ",
+        significado: "prep.: para cima; acima, sobre; cada; como prefixo: para cima; outra vez",
+        ocorrencias: 13
+    },
+    {
+        grego: "ἀπό (ἀπ᾽, ἀφ᾽)",
+        transliteracao: "a-PÓ",
+        significado: "prep.: de, desde; embora; da parte de",
+        ocorrencias: 646
+    },
+    {
+        grego: "διά (δι᾽)",
+        transliteracao: "di-Á",
+        significado: "prep.: por; por meio de; através de; por causa de",
+        ocorrencias: 668
+    },
+    {
+        grego: "ἐπί (ἐπ᾽, ἐφ᾽)",
+        transliteracao: "e-PÍ",
+        significado: "prep.: sobre, em, em cima de; acima; no tempo de, durante; para",
+        ocorrencias: 891
+    },
+    {
+        grego: "κατά (κατ᾽, καθ᾽)",
+        transliteracao: "ka-TÁ",
+        significado: "prep.: sobre; por; para baixo; contra; segundo, conforme",
+        ocorrencias: 476
+    },
+    {
+        grego: "παρά (παρ᾽)",
+        transliteracao: "pa-RÁ",
+        significado: "prep.: de, da parte de; perto de, junto de, ao lado de; junto a, perto de, ao longo de",
+        ocorrencias: 194
+    },
+    {
+        grego: "πρός",
+        transliteracao: "PRÓS",
+        significado: "prep.: (necessário) para; perto de, junto a; para; em direção a; com",
+        ocorrencias: 699
     }
 ];
 
--- data/licao5.js
+++ data/licao5.js
@@ -1,16 +1,130 @@
 const vocabularioLicao5 = [
     {
-        "grego": "πνεῦμα",
-        "transliteracao": "pneuma",
-        "significado": "espírito, vento",
-        "ocorrencias": 385
+        grego: "ἄγγελος, -ου, ὁ",
+        transliteracao: "ÁN-gue-los, -u, rró",
+        significado: "anjo; mensageiro",
+        ocorrencias: 176
     },
     {
-        "grego": "καρδία",
-        "transliteracao": "kardia",
-        "significado": "coração",
-        "ocorrencias": 159
+        grego: "ἀδελφός, -οῦ, ὁ",
+        transliteracao: "a-del-FÓS, -û, rró",
+        significado: "irmão, irmão na fé, compatriota, próximo",
+        ocorrencias: 343
     },
+    {
+        grego: "ἄνθρωπος, -ου, ὁ",
+        transliteracao: "ÁN-thro-pos, -u, rró",
+        significado: "homem; ser humano, pessoa",
+        ocorrencias: 551
+    },
+    {
+        grego: "δοῦλος, -ου, ὁ",
+        transliteracao: "DÛ-los, -u, rró",
+        significado: "escravo; servo",
+        ocorrencias: 124
+    },
+    {
+        grego: "ἔργον, -ου, τό",
+        transliteracao: "ÉR-gon, -u, tó",
+        significado: "trabalho; obra, ato, ação",
+        ocorrencias: 169
+    },
+    {
+        grego: "θάνατος, -ου, ὁ",
+        transliteracao: "THÁ-na-tos, -u, rró",
+        significado: "morte",
+        ocorrencias: 120
+    },
+    {
+        grego: "λαός, -οῦ, ὁ",
+        transliteracao: "la-ÓS, -û, rró",
+        significado: "povo",
+        ocorrencias: 142
+    },
+    {
+        grego: "νόμος, -ου, ὁ",
+        transliteracao: "NÓ-mos, -u, rró",
+        significado: "lei",
+        ocorrencias: 195
+    },
+    {
+        grego: "ὁδός, -οῦ, ἡ",
+        transliteracao: "ro-DÓS, -û, re",
+        significado: "caminho",
+        ocorrencias: 101
+    },
+    {
+        grego: "οὐρανός, -οῦ, ὁ",
+        transliteracao: "u-ra-NÓS, -û, rró",
+        significado: "céu",
+        ocorrencias: 274
+    },
+    {
+        grego: "ὀφθαλμός, -οῦ, ὁ",
+        transliteracao: "of-thal-MÓS, -û, rró",
+        significado: "olho",
+        ocorrencias: 100
+    },
+    {
+        grego: "ὄχλος, -ου, ὁ",
+        transliteracao: "Ó-khlos, -u, rró",
+        significado: "multidão; povo",
+        ocorrencias: 175
+    },
+    {
+        grego: "γίνομαι",
+        transliteracao: "GUÍ-no-mai",
+        significado: "ser; vir a ser, tornar-se; acontecer",
+        ocorrencias: 670
+    },
+    {
+        grego: "εἶδον / εἶδα",
+        transliteracao: "ÊI-don / ÊI-da",
+        significado: "ver; perceber [2º aor. de ὁράω]",
+        ocorrencias: 0 // Ocorrências contadas no verbo principal ὁράω
+    },
+    {
+        grego: "εἶπον / εἶπα",
+        transliteracao: "ÊI-pon / ÊI-pa",
+        significado: "dizer, falar [2º aor. de λέγω]",
+        ocorrencias: 0 // Ocorrências contadas no verbo principal λέγω
+    },
+    {
+        grego: "ἔρχομαι",
+        transliteracao: "ÉR-kho-mai",
+        significado: "ir; vir",
+        ocorrencias: 636
+    },
+    {
+        grego: "ὁράω",
+        transliteracao: "ro-RÁ-o",
+        significado: "ver; perceber; contemplar",
+        ocorrencias: 449
+    },
+    {
+        grego: "ἀμήν",
+        transliteracao: "a-MÉN",
+        significado: "partíc. asseverativa: verdadeiramente, certamente; amém, assim seja!",
+        ocorrencias: 130
+    },
+    {
+        grego: "ὅτι",
+        transliteracao: "HÓ-ti",
+        significado: "conj.: que; de modo que; pois, porque, visto que",
+        ocorrencias: 1297
+    },
+    {
+        grego: "μετά (μετ᾽, μεθ᾽)",
+        transliteracao: "me-TÁ",
+        significado: "prep.: com, junto a; entre; depois de, após",
+        ocorrencias: 473
+    },
+    {
+        grego: "περί",
+        transliteracao: "pe-RÍ",
+        significado: "prep.: concernente a, acerca de, sobre; ao redor de, em volta de",
+        ocorrencias: 333
+    }
 ];
 
 export { vocabularioLicao5 };
