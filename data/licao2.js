c32e87fb412261108d3941aa371909cc0ddbc894
Rodrigo Costa <rdgporto@gmail.com>
2025-09-23 10:00:26 -0300

    lição2

--- data/licao2.js
+++ data/licao2.js
@@ -1,16 +1,108 @@
+// data/licao02.js
 const vocabularioLicao2 = [
     {
-        "grego": "ἀγάπη",
-        "transliteracao": "agape",
-        "significado": "amor",
-        "ocorrencias": 117
+        grego: "ἐγώ",
+        transliteracao: "e-GÓ",
+        significado: "pron. pess. da 1ª pess. sg.: eu",
+        ocorrencias: 1802
     },
     {
-        "grego": "λόγος",
-        "transliteracao": "logos",
-        "significado": "palavra, razão",
-        "ocorrencias": 330
+        grego: "σύ",
+        transliteracao: "SÍ",
+        significado: "pron. pess. da 2ª pess. sg.: tu",
+        ocorrencias: 1066
     },
+    {
+        grego: "αὐτός, -ή, -ό",
+        transliteracao: "au-TÓS, -é, -ó",
+        significado: "pron. pess. da 3ª pess. sg.: ele, ela; ele(a) mesmo(a); o(a) mesmo(a)",
+        ocorrencias: 5601
+    },
+    {
+        grego: "ἡμεῖς",
+        transliteracao: "re-MÊIS",
+        significado: "pron. pess. da 1ª pess. pl.: nós",
+        ocorrencias: 864
+    },
+    {
+        grego: "ὑμεῖς",
+        transliteracao: "ri-MÊIS",
+        significado: "pron. pess. da 2ª pess. pl.: vós",
+        ocorrencias: 1840
+    },
+    {
+        grego: "ἀκολουθέω",
+        transliteracao: "a-ko-lu-THÉ-o",
+        significado: "seguir, acompanhar; ir junto com",
+        ocorrencias: 90
+    },
+    {
+        grego: "ἀκούω",
+        transliteracao: "a-KÚ-o",
+        significado: "ouvir; escutar; prestar atenção; entender; obedecer",
+        ocorrencias: 430
+    },
+    {
+        grego: "βαπτίζω",
+        transliteracao: "bap-TÍ-dzo",
+        significado: "batizar; imergir; mergulhar; lavar",
+        ocorrencias: 77
+    },
+    {
+        grego: "γινώσκω",
+        transliteracao: "gui-NÓS-ko",
+        significado: "vir a conhecer; conhecer, saber; compreender; reconhecer",
+        ocorrencias: 222
+    },
+    {
+        grego: "δίδωμι",
+        transliteracao: "DÍ-do-mi",
+        significado: "dar, outorgar, conceder; constituir; oferecer; causar; etc.",
+        ocorrencias: 415
+    },
+    {
+        grego: "εἰμί",
+        transliteracao: "ei-MÍ",
+        significado: "ser, estar; existir; acontecer; haver",
+        ocorrencias: 2461
+    },
+    {
+        grego: "λέγω",
+        transliteracao: "LÉ-go",
+        significado: "dizer, falar",
+        ocorrencias: 2282
+    },
+    {
+        grego: "λύω",
+        transliteracao: "LÍ-o",
+        significado: "desatar, soltar, libertar; destruir",
+        ocorrencias: 42
+    },
+    {
+        grego: "εἰς (ἐς)",
+        transliteracao: "EIS (es)",
+        significado: "prep.: para, a; para dentro; em; até",
+        ocorrencias: 1768
+    },
+    {
+        grego: "ἐκ (ἐξ)",
+        transliteracao: "EK (ex)",
+        significado: "prep.: de; fora de; a partir de; por",
+        ocorrencias: 916
+    },
+    {
+        grego: "ἐν",
+        transliteracao: "EN",
+        significado: "prep.: em, dentro de; no meio de; entre; com; por",
+        ocorrencias: 2757
+    },
+    {
+        grego: "καί",
+        transliteracao: "KÁI",
+        significado: "conj. copulativa: e; mas; como adv.: também",
+        ocorrencias: 9164
+    }
 ];
 
-export { vocabularioLicao2 };
+// Exporta o array como o padrão do módulo
+export default vocabularioLicao2;
